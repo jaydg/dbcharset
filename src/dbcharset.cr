b972f34666e5ed698b09200df06f419ba0d4b2f2
Joachim de Groot <jdegroot@web.de>
2017-05-10 15:06:54 +0200

    Move code into separate file

--- src/dbcharset.cr
+++ src/dbcharset.cr
@@ -14,38 +14,6 @@ class DBCharset
     @collation = ""
   end
 
-  private def get_tables(db)
-    tables = [] of String
-    q = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES
-          WHERE TABLE_SCHEMA=(?)
-            AND TABLE_TYPE='BASE TABLE'"
-
-    db.query q, @database do |result|
-      result.each do
-        tables << result.read(String)
-      end
-    end
-    return tables
-  end
-
-  private def get_columns(db, table)
-    columns = {} of String => String
-    q = "SELECT COLUMN_NAME, COLUMN_TYPE
-          FROM INFORMATION_SCHEMA.COLUMNS
-          WHERE COLLATION_NAME is not null
-            AND TABLE_SCHEMA=(?)
-            AND TABLE_NAME=(?)
-            AND (CHARACTER_SET_NAME != (?)
-              OR COLLATION_NAME != (?))"
-
-    db.query q, @database, table, @charset, @collation do |result|
-      result.each do
-        columns[result.read(String)] = result.read(String)
-      end
-    end
-    return columns
-  end
-
   def convert(db, tables)
     bar = ProgressBar.new
     bar.width = 60
@@ -67,31 +35,6 @@ class DBCharset
 
     end
     db.exec "SET foreign_key_checks = 1"
-  end
-
-  def connect
-    uri = "mysql://"
-    uri += "#{@user}#{':' unless @user.empty? || @password.empty?}#{@password}"
-    uri += "#{'@' unless @user.empty? }#{@host}:#{@port}/#{@database}"
-    db = DB.open uri
-
-    if @charset.empty?
-      # Get the default charset configured for the database
-      q = "SELECT default_character_set_name
-           FROM information_schema.SCHEMATA
-           WHERE SCHEMA_NAME = (?)"
-      @charset = db.query_one q, @database, as: String
-    end
-
-    if @collation.empty?
-      # Get the default collation configured for the database
-      q = "SELECT DEFAULT_COLLATION_NAME
-           FROM information_schema.SCHEMATA
-           WHERE SCHEMA_NAME = (?)"
-      @collation = db.query_one q, @database, as: String
-    end
-
-    return db
   end
 
   def run
